bc237067b06c0a746548bafa6cb1a51bb8369c8d
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2024-12-14 01:01:23 +0300

    organize

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use game::{Game, Player};
 pub mod game {
     use std::{collections::HashSet, io};
 
-    // use board::Board;
-    pub type BoardRow = Vec<BoardCell>;
-    pub type BoardCell = Option<Player>;
-    pub type Board = Vec<BoardRow>;
+    use board::{Board, BoardCell};
 
     pub enum GameStatus {
         Ongoing,
@@ -23,8 +20,8 @@ pub mod game {
 
     #[derive(Clone, PartialEq)]
     pub struct Player {
-        pub name: String,
-        pub token: char,
+        name: String,
+        token: char,
     }
 
     impl Player {
@@ -51,8 +48,12 @@ pub mod game {
 
             Self::validate_players(&players);
 
+            if let Err(err) = Self::validate_game_config(row_count, col_count, tokens_to_win) {
+                panic!("Invalid game configuration: {}", err);
+            }
+
             Self {
-                board: generate_board(row_count, col_count),
+                board: Board::new(row_count, col_count),
                 tokens_to_win,
                 players,
                 current_turn: 0,
@@ -74,42 +75,22 @@ pub mod game {
             }
         }
 
-        fn display_board(&self) {
-            for col in 0..self.board[0].len() {
-                print!(" {} ", col);
+        fn validate_game_config(
+            rows: usize,
+            cols: usize,
+            tokens_to_win: usize,
+        ) -> Result<(), String> {
+            if rows < 1 || cols < 1 {
+                return Err("Rows and columns must be greater than 0.".to_string());
+            }
+            if tokens_to_win < 2 {
+                return Err("Tokens to win must be at least 2.".to_string());
+            }
+            if tokens_to_win > rows || tokens_to_win > cols {
+                return Err("Tokens to win cannot be greater than rows or columns.".to_string());
             }
 
-            println!();
-
-            for row in &self.board {
-                for col in row {
-                    match col {
-                        Some(player) => print!("[{}]", player.token),
-                        None => print!("[ ]"),
-                    }
-                }
-
-                println!();
-            }
-        }
-
-        fn is_board_full(&self) -> bool {
-            for row in &self.board {
-                if row.iter().any(|cell| cell.is_none()) {
-                    return false;
-                }
-            }
-
-            true
-        }
-
-        fn valid_move(&self, col: usize) -> bool {
-            if col >= self.board[0].len() {
-                return false;
-            }
-
-            // check if column is full
-            self.board.iter().any(|row| row[col].is_none())
+            Ok(())
         }
 
         fn get_valid_input(&self) -> usize {
@@ -123,58 +104,16 @@ pub mod game {
                 }
 
                 match input_line.trim().parse::<usize>() {
-                    Ok(value) if self.valid_move(value) => return value,
+                    Ok(value) if self.board.valid_move(value) => return value,
                     Ok(_) => println!("Invalid move. Column is either full or out of range."),
                     Err(_) => println!("Invalid input. Please enter a valid integer."),
                 }
             }
         }
 
-        fn get_diagonals_top_right_to_bottom_left(&self) -> Board {
-            let mut diagonals = Vec::new();
-            let rows = self.board.len();
-            let cols = self.board[0].len();
-
-            for d in 0..(rows + cols - 1) {
-                let mut diagonal = Vec::new();
-                for i in 0..rows {
-                    let j = (cols as isize - 1) - (d as isize - i as isize);
-                    if j >= 0 && (j as usize) < cols {
-                        diagonal.push(self.board[i][j as usize].clone());
-                    }
-                }
-                if !diagonal.is_empty() {
-                    diagonals.push(diagonal);
-                }
-            }
-
-            diagonals
-        }
-
-        fn get_diagonals_top_left_to_bottom_right(&self) -> Board {
-            let mut diagonals = Vec::new();
-            let rows = self.board.len();
-            let cols = self.board[0].len();
-
-            for d in 0..(rows + cols - 1) {
-                let mut diagonal = Vec::new();
-                for i in 0..rows {
-                    let j = d as isize - i as isize;
-                    if j >= 0 && (j as usize) < cols {
-                        diagonal.push(self.board[i][j as usize].clone());
-                    }
-                }
-                if !diagonal.is_empty() {
-                    diagonals.push(diagonal);
-                }
-            }
-
-            diagonals
-        }
-
         fn check_line(line: &[BoardCell], tokens_to_win: usize) -> BoardCell {
             let mut count = 0;
-            let mut last_player: Option<Player> = None;
+            let mut last_player: BoardCell = None;
 
             for cell in line {
                 if let Some(player) = cell {
@@ -198,29 +137,29 @@ pub mod game {
 
         fn find_winner(&self) -> BoardCell {
             // Check rows for winner
-            for row in &self.board {
+            for row in &self.board.rows {
                 if let Some(winner) = Self::check_line(row, self.tokens_to_win) {
                     return Some(winner);
                 }
             }
 
             // Check columns for winner
-            for col in 0..self.board[0].len() {
-                let column: Vec<_> = self.board.iter().map(|row| row[col].clone()).collect();
+            for col in 0..self.board.rows[0].len() {
+                let column: Vec<_> = self.board.rows.iter().map(|row| row[col].clone()).collect();
                 if let Some(winner) = Self::check_line(&column, self.tokens_to_win) {
                     return Some(winner);
                 }
             }
 
             // Check top-left to bottom-right diagonals for winner
-            for diagonal in self.get_diagonals_top_left_to_bottom_right() {
+            for diagonal in self.board.get_diagonals_top_left_to_bottom_right() {
                 if let Some(winner) = Self::check_line(&diagonal, self.tokens_to_win) {
                     return Some(winner);
                 }
             }
 
             // Check top-right to bottom-left diagonals for winner
-            for diagonal in self.get_diagonals_top_right_to_bottom_left() {
+            for diagonal in self.board.get_diagonals_top_right_to_bottom_left() {
                 if let Some(winner) = Self::check_line(&diagonal, self.tokens_to_win) {
                     return Some(winner);
                 }
@@ -231,7 +170,7 @@ pub mod game {
         }
 
         fn status(&self) -> GameStatus {
-            if self.is_board_full() {
+            if self.board.is_board_full() {
                 return GameStatus::Draw;
             }
 
@@ -241,21 +180,9 @@ pub mod game {
             }
         }
 
-        fn place_token(&mut self, col: usize, player: Player) {
-            let mut target_row = 0;
-
-            for (i, row) in self.board.iter().enumerate() {
-                if row[col].is_none() {
-                    target_row = i;
-                }
-            }
-
-            self.board[target_row][col] = Some(player);
-        }
-
         pub fn start(&mut self) {
             loop {
-                self.display_board();
+                println!("{}", self.board.display());
 
                 match self.status() {
                     GameStatus::Ongoing => (),
@@ -275,7 +202,8 @@ pub mod game {
                 );
 
                 let input_col = self.get_valid_input();
-                self.place_token(input_col, self.players[self.current_turn].clone());
+                self.board
+                    .place_token(input_col, self.players[self.current_turn].clone());
 
                 // chance turn
                 self.next_turn();
@@ -283,32 +211,127 @@ pub mod game {
         }
     }
 
-    fn generate_board(row_count: usize, col_count: usize) -> Board {
-        vec![vec![None; col_count]; row_count]
-    }
+    mod board {
+        use super::Player;
 
-    // mod board {
-    //     use super::Player;
-    //
-    //     pub type BoardRow = Vec<BoardCell>;
-    //     pub type BoardCell = Option<Player>;
-    //
-    //     pub struct Board {
-    //         pub rows: BoardRow,
-    //     }
-    //
-    //     impl Board {
-    //         pub fn new(row_count: usize, col_count: usize) -> Board {
-    //             vec![vec![None; col_count]; row_count]
-    //         }
-    //     }
-    // }
+        pub type BoardRow = Vec<BoardCell>;
+        pub type BoardCell = Option<Player>;
+
+        pub struct Board {
+            pub rows: Vec<BoardRow>,
+        }
+
+        impl Board {
+            pub fn new(row_count: usize, col_count: usize) -> Self {
+                Self {
+                    rows: vec![vec![None; col_count]; row_count],
+                }
+            }
+
+            pub fn display(&self) -> String {
+                let mut output = String::new();
+
+                for col in 0..self.rows[0].len() {
+                    output.push_str(&format!(" {} ", col));
+                }
+
+                output.push('\n');
+
+                for row in &self.rows {
+                    for col in row {
+                        let symbol = match col {
+                            Some(player) => player.token,
+                            None => ' ',
+                        };
+                        output.push_str(&format!("[{}]", symbol));
+                    }
+
+                    output.push('\n');
+                }
+
+                output
+            }
+
+            pub fn place_token(&mut self, col: usize, player: Player) {
+                let mut target_row = 0;
+
+                for (i, row) in self.rows.iter().enumerate() {
+                    if row[col].is_none() {
+                        target_row = i;
+                    }
+                }
+
+                self.rows[target_row][col] = Some(player);
+            }
+
+            pub fn get_diagonals_top_right_to_bottom_left(&self) -> Vec<Vec<BoardCell>> {
+                let mut diagonals = Vec::new();
+                let rows = self.rows.len();
+                let cols = self.rows[0].len();
+
+                for d in 0..(rows + cols - 1) {
+                    let mut diagonal = Vec::new();
+                    for i in 0..rows {
+                        let j = (cols as isize - 1) - (d as isize - i as isize);
+                        if j >= 0 && (j as usize) < cols {
+                            diagonal.push(self.rows[i][j as usize].clone());
+                        }
+                    }
+                    if !diagonal.is_empty() {
+                        diagonals.push(diagonal);
+                    }
+                }
+
+                diagonals
+            }
+
+            pub fn get_diagonals_top_left_to_bottom_right(&self) -> Vec<Vec<BoardCell>> {
+                let mut diagonals = Vec::new();
+                let rows = self.rows.len();
+                let cols = self.rows[0].len();
+
+                for d in 0..(rows + cols - 1) {
+                    let mut diagonal = Vec::new();
+                    for i in 0..rows {
+                        let j = d as isize - i as isize;
+                        if j >= 0 && (j as usize) < cols {
+                            diagonal.push(self.rows[i][j as usize].clone());
+                        }
+                    }
+                    if !diagonal.is_empty() {
+                        diagonals.push(diagonal);
+                    }
+                }
+
+                diagonals
+            }
+
+            pub fn valid_move(&self, col: usize) -> bool {
+                if col >= self.rows[0].len() {
+                    return false;
+                }
+
+                // check if column is full
+                self.rows.iter().any(|row| row[col].is_none())
+            }
+
+            pub fn is_board_full(&self) -> bool {
+                for row in &self.rows {
+                    if row.iter().any(|cell| cell.is_none()) {
+                        return false;
+                    }
+                }
+
+                true
+            }
+        }
+    }
 }
 
 fn main() {
-    let players = vec![Player::new("Bob", 'x'), Player::new("Sarah", 'o')];
+    let players = vec![Player::new("Raymond", 'x'), Player::new("Anaïs", 'o')];
 
-    let mut game = Game::new(5, 7, 4, players);
+    let mut game = Game::new(5, 5, 4, players);
 
     game.start();
 }
