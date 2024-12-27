6a1d959c55be2912f8e0609ba92db65cb6db8002
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2024-12-27 15:44:10 +0300

    add rustdoc documentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+//! # Connect Four CLI Game
+//!
+//! ## Introduction
+//!
+//! Connect Four is a classic two-player connection game. Players take turns selecting a column to drop their colored discs into a vertically suspended grid with seven columns and six rows. The discs fall straight down, occupying the lowest available space in the chosen column.
+//!
+//! The objective of the game is to be the first to form a line of four discs horizontally, vertically, or diagonally.
+//!
+//! This implementation provides a command-line interface (CLI) version of the game. It supports up to 26 players and allows for customizable board dimensions. The default board size is 6x7. Each player is assigned a unique token (character), starting with `a` for the first player, followed by `b`, `c`, and so on for subsequent players.
+//!
+//! ## Usage
+//!
+//! Run the game with the following command:
+//!
+//! ```bash
+//! connect_four [OPTIONS]
+//! ```
+//!
+//! ### Options:
+//!
+//! - `-p`, `--players <PLAYERS> <PLAYERS>...`
+//!   Specify the players participating in the game.
+//!
+//! - `-r`, `--rows <ROWS>`
+//!   Set the number of rows on the board. [default: 6]
+//!
+//! - `-c`, `--cols <COLS>`
+//!   Set the number of columns on the board. [default: 7]
+//!
+//! - `-t`, `--tokens-to-win <TOKENS_TO_WIN>`
+//!   Specify the number of connected tokens required to win. [default: 4]
+//!
+//! - `-h`, `--help`
+//!   Display usage information.
+//!
+//! ## Roadmap
+//!
+//! Planned features for future development include:
+//!
+//! - [ ] WebSocket support for online multiplayer functionality.
+//! - [ ] An AI opponent to play against.
+//!
+//! ## Examples
+//!
+//! ```
+//! let players = vec![
+//!    Player::new("Alice"),
+//!    Player::new("Bob"),
+//! ]
+//!
+//! let rows = 6;
+//! let cols = 7;
+//! let tokens_to_win = 4;
+//!
+//! let mut game = Game::new(rows, cols, tokens_to_win, players);
+//! game.start();
+//! ```
+
 use clap::Parser;
 
 mod game;
@@ -8,7 +66,7 @@ pub struct Args {
     #[arg(short, long, value_parser, num_args = 2..=26, value_delimiter = ' ')]
     pub players: Vec<String>,
 
-    #[arg(short, long, default_value = "5")]
+    #[arg(short, long, default_value = "6")]
     pub rows: usize,
 
     #[arg(short, long, default_value = "7")]
@@ -27,7 +85,5 @@ fn main() {
         .map(|name| Player::new(name))
         .collect::<Vec<Player>>();
 
-    let mut game = Game::new(args.rows, args.cols, args.tokens_to_win, players);
-
-    game.start();
+    Game::new(args.rows, args.cols, args.tokens_to_win, players).start();
 }
